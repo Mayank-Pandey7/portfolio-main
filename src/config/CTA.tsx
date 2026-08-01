bcb047776b25dba43163eff3bf78a8e87f1ec4e8
Mayank Pandey <mayankpandey0717@gmail.com>
2026-08-01 16:56:10 +0530

    with Mayank, fix project mdx files

--- src/config/CTA.tsx
+++ src/config/CTA.tsx
@@ -12,7 +12,7 @@ export const ctaConfig = {
 
   // CTA button settings
   linkText: 'Book a Free Call',
-  calLink: 'mynkdev/meeting',
+  calLink: 'mayank-pandey/meeting',
 
   // Pre-text message
   preText: "Hey, you scrolled this far, let's talk.",
